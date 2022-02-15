55043a27db52cd24d12748e9bf529e5facf59ed3
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-02-16 00:25:00 +0530

    concat done

--- String.js
+++ String.js
@@ -5,5 +5,27 @@ console.log(str.length);
 
 // 1. Extraction Method
 //  slice
-let slicestr=str.slice(0,4)
-console.log(slicestr);
+let slicestr=str.slice(0,-4)
+
+//  Substring Method
+
+let ans=str.substr(2,4)
+console.log(ans);
+console.log(str);
+
+//  replacing
+var hello="YOLO"
+var hello1="YOLO"
+console.log(hello);
+hello.toLowerCase() 
+hello.toUpperCase() 
+console.log(hello.toLowerCase());
+console.log(hello1.toUpperCase());
+
+let str1="kesa h"
+let str2="thik h bhai"
+let cons=str1+str2
+console.log(cons);
+
+let contactstr=str1.concat(str2)
+console.log(contactstr);
