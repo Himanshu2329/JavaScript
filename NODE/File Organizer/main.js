7e6671f4be86feca85d14f060673749d5effabf9
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-04-01 14:14:22 +0530

    .join

---
+++ NODE/File Organizer/main.js
@@ -0,0 +1,29 @@
+//  entry point of my command line
+let helpfuntion=require("./help");
+console.log(helpfuntion.hathi());
+let inputArr=process.argv.slice(2);
+let command=inputArr[0];
+let path=inputArr[1];
+switch (command) {
+    case "tree":
+        // call tree function
+        console.log("Tree Function Called and Executed Successfully on  path "+path);
+
+        
+        break;
+    case "organize":
+        // call organize function
+        console.log("Organized Function Called and Executed Successfully"+path);
+
+        break;
+    case "help":
+        // call help function
+        helpfuntion.hathi();
+        // console.log("Help Function Called and Executed Successfully"+path);
+
+        break;
+
+    default:
+        console.log("command not recognized ");
+        break;
+}
---
+++ NODE/File Organizer/organize.js
@@ -0,0 +1,28 @@
+const fs=require("fs"); //fs module
+
+const path=require("path"); //path module
+
+
+let types={
+    media:["mp4","mkv","mp3"],
+    archives:['zip','7z','rar','tar','gz','iso','xz','ar'],
+    document:['docx','doc','pdf','xlsx','xls','odt','ods','odp','odg','odf','txt','ps','tex'],
+    app:['exe','bat','dmg','pkg','deb','dll'],
+    images:['jpeg','gif','jpg','png']
+}
+
+organize=(srcPath)=>{
+    if (srcPath==undefined) {
+        console.log(srcPath);
+        // The process.cwd() method returns the CWD(current working directory) of the Node.js process.
+        srcPath=process.cwd();
+        console.log("source path is ",srcPath);
+    }
+
+    let organizedPath= path.join(srcPath,"Organized_Files");
+    console.log("organized files ke folder ka path",organizedPath);
+    fs.mkdirSync(organizedPath)
+    // else console.log(srcPath);
+}
+// organize("./wcat/wcat.js");
+organize();
