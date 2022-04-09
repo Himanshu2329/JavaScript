c75b1ac50cdee801aa46818c9e549a7133f4749d
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-04-09 17:12:16 +0530

    90% done

--- NODE/File Organizer/organize.js
+++ NODE/File Organizer/organize.js
@@ -1,7 +1,8 @@
 const fs = require("fs"); //fs module
+const { basename } = require("path");
 
 const path = require("path"); //path module
-let getFold=require("./Function")
+// let getFold=require("./Function")
 
 
 let types = {
@@ -25,7 +26,7 @@ organize = (srcPath) => {
     console.log("organized files ke folder ka path", organizedPath);
     if (fs.existsSync(organizedPath) == false) {
         // exist nhi krta to folder bna do vrna rehnedo
-        // fs.mkdirSync(organizedPath)
+        fs.mkdirSync(organizedPath)
     }
     else console.log("already exist");
 
@@ -33,7 +34,7 @@ organize = (srcPath) => {
 
     // only reads the content of the file(only names)
     let allFiles = fs.readdirSync(srcPath);
-    // console.log(allFiles);
+    console.log(allFiles);
 
 
     //4.  traverse over the files and classify them on the basis of their extension
@@ -47,7 +48,7 @@ organize = (srcPath) => {
         // 1. check if it is a file or a folder
         //  lstatsync gives the information regarding the link provided
         let isFile = fs.lstatSync(fullPathofFile).isFile();
-        console.log(allFiles[i]+" is "+isFile);
+        // console.log(allFiles[i]+" is "+isFile);
 
         if (isFile) {
 
@@ -59,25 +60,48 @@ organize = (srcPath) => {
 
 
             // 1.2 get folder name from extension
-            let folder_name = getFolderName(extenFile); //archive aayega
-            
-            
+            let folder_name = getFolderName(extenFile); //archive aayega 
+            console.log(folder_name);
+
+
             // 1.3 copy from src folder(srcPath) and paste in dest folder(folder_name eg:-document,media etc)
 
-                 
+
             copyFileToDest(srcPath, fullPathofFile, folder_name);
         }
     }
 
 
 }
-getFolderName=(srcPath)=>{
-
-    return folder_name;
+getFolderName = (extenFile) => {
+    for (let key in types) {
+        // console.log(key);
+        for (let i = 0; i < types[key].length; i++) {
+            if (types[key][i] == extenFile) {
+                return key;
+            }
+        }
+    }
+    // return folder_name;
 }
 
-copyFileToDest=(srcPath, fullPathofFile, folder_name)=>{
-    
+copyFileToDest = (srcPath, fullPathofFile, folder_name) => {
+    // 1. foolderName ka path bnana padega 
+    let destFolder = path.join(srcPath, "Organized_Files", folder_name);
+    console.log(destFolder);
+    // 2. make folder if exists, if it does not, then make it
+    if (fs.existsSync(destFolder) == false) {
+        fs.mkdirSync(destFolder);
+    }
+    // 3. copy file from src folder to dest folder
+
+    // basename=Return the last portion of a path. Similar to the Unix basename command. Often used to extract the file name from a fully qualified path.
+    let FileName = path.basename(fullPathofFile); // abc.zip
+    let destFileName = path.join(destFolder, FileName)  
+                                // source  , Destination
+
+    fs.copyFileSync(fullPathofFile, destFileName);
+
     // magic will happen bro
 }
 
