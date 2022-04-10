dd154242a355f8d53f1040a605ba4874471fd39d
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-04-10 20:43:39 +0530

    corona Detail

--- NODE/Web Scrapping/CoronoDetails.js
+++ NODE/Web Scrapping/CoronoDetails.js
@@ -1,3 +1,4 @@
+const cheerio  = require("cheerio");
 const request=require("request")
 
 request("https://www.worldometers.info/coronavirus/", cb)
@@ -5,9 +6,25 @@ request("https://www.worldometers.info/coronavirus/", cb)
 
 
 function cb(err,res,body){
-    console.log("error",err);
+    if(err){
+
+        console.log("error",err);
+    }
+    else{
+        handleHtml(body)
+    }
     // console.log(res);
-    console.log(body);
+    // console.log(body);
     // extract krle data html se
 }
-console.log("After");
+function handleHtml(html) {
+    let selecTool=cheerio.load(html)
+    let coronaStat=selecTool(".maincounter-number")
+    // console.log(coronaStat.text());
+    let totalCases=selecTool(coronaStat[0]).text()
+    console.log("total cases-> "+totalCases);
+    let totalDeath=selecTool(coronaStat[1]).text()
+    console.log("total Death-> "+totalDeath);
+    let totalRec=selecTool(coronaStat[2]).text()
+    console.log("total Recoveries-> "+totalRec);
+}
--- NODE/Web Scrapping/request.js
+++ NODE/Web Scrapping/request.js
@@ -1,8 +1,10 @@
 const request=require("request")
 request("https://www.worldometers.info/coronavirus/", cb)
-
+console.log("before");
 function cb(err,res,body){
     console.log("error",err);
     // console.log(res);
-    console.log(body);
+    // console.log(body);
+    console.log(typeof body);
 }
+console.log("After");
